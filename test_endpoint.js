164bc7931f8ca2aeed3654b133db59695541dd06
Luke Karrys <luke@lukekarrys.com>
2023-04-30 11:34:49 -0700

    prettier

--- test_endpoint.js
+++ test_endpoint.js
@@ -1,27 +1,27 @@
-"use strict";
-const http = require("http");
+'use strict'
+const http = require('http')
 
 let server = http.createServer((req, res) => {
-  console.log(req.method, req.url);
+  console.log(req.method, req.url)
   for (let header in req.headers) {
-    console.log(header + ":", req.headers[header]);
+    console.log(header + ':', req.headers[header])
   }
 
-  console.log("");
+  console.log('')
 
-  const buffer = [];
+  const buffer = []
 
-  req.on("data", (data) => buffer.push(data.toString()));
-  req.on("end", () => {
-    res.end();
+  req.on('data', (data) => buffer.push(data.toString()))
+  req.on('end', () => {
+    res.end()
 
     try {
-      const json = JSON.parse(buffer.join(""));
-      console.dir(json, { depth: 10 });
-      console.log("");
+      const json = JSON.parse(buffer.join(''))
+      console.dir(json, { depth: 10 })
+      console.log('')
     } catch (e) {}
-  });
-});
+  })
+})
 
-server.listen(5007);
-console.log("Listening on http://localhost:5007/");
+server.listen(5007)
+console.log('Listening on http://localhost:5007/')
